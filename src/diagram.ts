56442206a9740685f10543c2e7159fcf9815b7a1
Matthijs Groen <matthijs.groen@gmail.com>
2023-04-17 13:53:19 +0200

    simplified diagram

--- src/diagram.ts
+++ src/diagram.ts
@@ -3,91 +3,82 @@ import g from "./game";
 type MetaData = {
   state: "text" | "progress" | "images" | "animated";
   languages: "en" | "nl";
-  location:
-    | "darkWood"
-    | "farmland"
-    | "forest"
-    | "hills"
-    | "home"
-    | "river"
-    | "swamp"
-    | "village";
 };
 
 g.definePuzzleDependencies<MetaData>({
   getOutOfBed: {
-    tags: { location: "home" },
+    tags: {},
     hierarchy: ["home"],
   },
   openWindow: {
     dependsOn: ["getOutOfBed"],
-    tags: { location: "home" },
+    tags: {},
     hierarchy: ["home"],
   },
   getBagFromDoor: {
     dependsOn: ["openWindow"],
-    tags: { location: "home" },
+    tags: {},
     hierarchy: ["home"],
   },
   getKeyFromBag: {
     dependsOn: ["getBagFromDoor"],
-    tags: { location: "home" },
+    tags: {},
     hierarchy: ["home"],
   },
   getFoodFromCloset: {
     dependsOn: ["getKeyFromBag"],
-    tags: { location: "home" },
+    tags: {},
     hierarchy: ["home"],
   },
   feedRaven: {
     dependsOn: ["getFoodFromCloset"],
-    tags: { location: "home" },
+    tags: {},
     hierarchy: ["home"],
   },
   getLetterFromRaven: {
     dependsOn: ["feedRaven"],
-    tags: { location: "home" },
+    tags: {},
     hierarchy: ["home"],
   },
   readLetter: {
     dependsOn: ["getLetterFromRaven"],
-    tags: { location: "home" },
+    tags: {},
     hierarchy: ["home"],
   },
   mainAct: {
     type: "chapter",
     dependsOn: ["readLetter"],
-    tags: { location: "forest", state: "text" },
+    tags: { state: "text" },
   },
   getWood: {
     dependsOn: ["mainAct"],
-    tags: { location: "forest", state: ["text"], languages: ["en", "nl"] },
+    tags: { state: ["text"], languages: ["en", "nl"] },
     hierarchy: ["forest"],
   },
   payTwoCoins: {
     dependsOn: ["mainAct"],
-    tags: { location: "village", state: ["text"], languages: ["en", "nl"] },
+    tags: { state: ["text"], languages: ["en", "nl"] },
     hierarchy: ["village"],
   },
   buyCookies: {
     dependsOn: ["payTwoCoins", "get100CoinsFromFlour"],
     gateType: "or",
-    tags: { location: "village", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["village", "bakery"],
   },
   giveFood: {
     dependsOn: ["buyCookies", "talkToDwarf"],
-    tags: { location: "hills", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["hills", "mine"],
   },
   talkToDwarf: {
     dependsOn: ["mainAct"],
-    tags: { location: "hills", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["hills", "mine"],
   },
   getBrokenPickAxe: {
     dependsOn: ["talkToDwarf"],
-    tags: { location: "hills", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["hills", "mine"],
   },
   repairPickAxe: {
@@ -96,183 +87,183 @@ g.definePuzzleDependencies<MetaData>({
   },
   givePickAxe: {
     dependsOn: ["repairPickAxe"],
-    tags: { location: "hills", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["hills", "mine"],
   },
   getGem: {
     dependsOn: ["giveFood", "givePickAxe"],
-    tags: { location: "hills", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["hills", "mine"],
   },
   talkToBaker: {
     dependsOn: ["mainAct"],
-    tags: { location: "village", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["village", "bakery"],
   },
   talkToWitch: {
     dependsOn: ["getEntranceToSwamp"],
-    tags: { location: "swamp", state: "progress", languages: ["en", "nl"] },
+    tags: { state: "progress", languages: ["en", "nl"] },
     hierarchy: ["swamp", "cabin"],
   },
   createJewelryAtSmith: {
     dependsOn: ["getGem", "getGoldInForest", "talkToWitch"],
-    tags: { location: "village", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["village", "smithy"],
   },
   giveJewelryToWitch: {
     dependsOn: ["createJewelryAtSmith", "talkToWitch"],
-    tags: { location: "swamp", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["swamp", "cabin"],
   },
   getIngredientList: {
     dependsOn: ["giveJewelryToWitch"],
-    tags: { location: "swamp", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["swamp", "cabin"],
   },
   pluckMushrooms: {
     dependsOn: ["getIngredientList", "theMonster"],
-    tags: { location: "darkWood" },
+    tags: {},
     hierarchy: ["darkWood"],
   },
   pluckPlants: {
     dependsOn: ["getIngredientList"],
-    tags: { location: "swamp", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["swamp"],
   },
   getAccessToCabin: {
     dependsOn: ["pluckMushrooms", "pluckPlants", "getDragonTooth"],
-    tags: { location: "swamp" },
+    tags: {},
     hierarchy: ["swamp", "cabin"],
   },
   getBeaker: {
     dependsOn: ["getAccessToCabin"],
-    tags: { location: "swamp" },
+    tags: {},
     hierarchy: ["swamp", "cabin"],
   },
   dissolvePotion: {
     dependsOn: ["getAccessToCabin"],
-    tags: { location: "swamp" },
+    tags: {},
     hierarchy: ["swamp", "cabin"],
   },
   getAccessToBasement: {
     dependsOn: ["dissolvePotion"],
-    tags: { location: "darkWood" },
+    tags: {},
     hierarchy: ["darkWood", "tower"],
   },
   getWineFromCellar: {
     dependsOn: ["getBeaker", "getAccessToBasement"],
-    tags: { location: "darkWood" },
+    tags: {},
     hierarchy: ["darkWood", "tower"],
   },
   brewWakeUpPotion: {
     dependsOn: ["getWineFromCellar"],
-    tags: { location: "swamp" },
+    tags: {},
     hierarchy: ["darkWood", "tower"],
   },
   wakeUpWitch: {
     dependsOn: ["brewWakeUpPotion"],
-    tags: { location: "swamp" },
+    tags: {},
     hierarchy: ["swamp", "cabin"],
   },
   createMedicine: {
     dependsOn: ["wakeUpWitch"],
-    tags: { location: "swamp" },
+    tags: {},
     hierarchy: ["swamp", "cabin"],
   },
   payFarrier: {
     dependsOn: ["payTwoCoins", "getCoinsFromSmith"],
     gateType: "or",
-    tags: { location: "village", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["village", "smithy"],
   },
   getHorse: {
     dependsOn: ["talkToFarmer"],
-    tags: { location: "river", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["river"],
   },
   getCoinsFromSmith: {
     dependsOn: ["createJewelryAtSmith"],
-    tags: { location: "village", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["village", "smithy"],
   },
   fixHorseshoe: {
     dependsOn: ["getHorse", "payFarrier"],
-    tags: { location: "village", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["village", "smithy"],
   },
   getGiantTrunks: {
     dependsOn: ["mainAct"],
-    tags: { location: "river", state: ["text"], languages: ["en", "nl"] },
+    tags: { state: ["text"], languages: ["en", "nl"] },
     hierarchy: ["river"],
   },
   repairMill: {
     dependsOn: ["getGiantTrunks"],
-    tags: { location: "hills" },
+    tags: { state: ["text"], languages: ["en", "nl"] },
     hierarchy: ["hills", "mill"],
   },
   getCart: {
     dependsOn: ["repairMill", "fixHorseshoe"],
-    tags: { location: "hills" },
+    tags: {},
     hierarchy: ["hills", "mill"],
   },
   putGrainOnCart: {
     dependsOn: ["getCart", "talkToFarmer"],
-    tags: { location: "hills" },
+    tags: {},
     hierarchy: ["farm"],
   },
   deliverGrainToMiller: {
     dependsOn: ["putGrainOnCart"],
-    tags: { location: "hills" },
+    tags: {},
     hierarchy: ["hills", "mill"],
   },
   getFlourFromMiller: {
     dependsOn: ["deliverGrainToMiller"],
-    tags: { location: "hills" },
+    tags: {},
     hierarchy: ["hills", "mill"],
   },
   deliverFlourToBaker: {
     dependsOn: ["getFlourFromMiller"],
-    tags: { location: "village" },
+    tags: {},
     hierarchy: ["farm"],
   },
   get100CoinsFromFlour: {
     dependsOn: ["deliverFlourToBaker"],
-    tags: { location: "village" },
+    tags: {},
     hierarchy: ["village", "bakery"],
   },
   buySword: {
     dependsOn: ["talkToBaker", "get100CoinsFromFlour"],
-    tags: { location: "village" },
+    tags: {},
     hierarchy: ["village", "smithy"],
   },
   talkToSmiths: {
     dependsOn: ["mainAct"],
-    tags: { location: "village", state: "progress", languages: ["en", "nl"] },
+    tags: { state: "progress", languages: ["en", "nl"] },
     hierarchy: ["village", "smithy"],
   },
   getTreasureSubject: {
     dependsOn: ["talkToSmiths"],
-    tags: { location: "village", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["village", "smithy"],
   },
   getPlantHintOfBaker: {
     dependsOn: ["getTreasureSubject"],
-    tags: { location: "village", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["village", "bakery"],
   },
   getRiverHintFromMiller: {
     dependsOn: ["getTreasureSubject"],
-    tags: { location: "hills", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["hills", "mill"],
   },
   getRouteHintFromFarmer: {
     dependsOn: ["getTreasureSubject"],
-    tags: { location: "farmland", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["farm"],
   },
   getEntranceToSwamp: {
     dependsOn: ["talkToBaker"],
-    tags: { location: "village", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["village", "bakery"],
   },
   completeTreasureRouteInfo: {
@@ -281,126 +272,126 @@ g.definePuzzleDependencies<MetaData>({
   },
   findMoonStoneInSwamp: {
     dependsOn: ["getPlantHintOfBaker", "getEntranceToSwamp"],
-    tags: { location: "swamp", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["swamp"],
   },
   followMoonStoneTrail: {
     dependsOn: ["findMoonStoneInSwamp", "completeTreasureRouteInfo"],
-    tags: { location: "river", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["river"],
   },
   getRuneStone: {
     dependsOn: ["followMoonStoneTrail"],
-    tags: { location: "forest", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["forest"],
   },
   talkToFarmer: {
     dependsOn: ["mainAct"],
-    tags: { location: "farmland", state: "progress", languages: ["en", "nl"] },
+    tags: { state: "progress", languages: ["en", "nl"] },
     hierarchy: ["farm"],
   },
   getRope: {
     dependsOn: ["mainAct"],
-    tags: { location: "farmland", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["farm"],
   },
   getInfoAboutMonster: {
     dependsOn: ["talkToBaker", "talkToFarmer"],
-    tags: { state: "text" },
+    tags: { state: "text", languages: ["en", "nl"] },
   },
   theMonster: {
     dependsOn: ["buySword", "getInfoAboutMonster"],
-    tags: { location: "darkWood" },
+    tags: {},
     type: "chapter",
   },
   climbTower: {
     dependsOn: ["theMonster"],
-    tags: { location: "darkWood" },
+    tags: {},
     hierarchy: ["darkWood", "tower"],
   },
   talkToDaughter: {
     dependsOn: ["climbTower"],
-    tags: { location: "darkWood" },
+    tags: {},
     hierarchy: ["darkWood", "tower"],
   },
   putMillstoneOnCart: {
     dependsOn: ["talkToDaughter"],
-    tags: { location: "hills" },
+    tags: {},
     hierarchy: ["hills", "mill"],
   },
   putMillstoneOnElevator: {
     dependsOn: ["putMillstoneOnCart", "talkToDaughter"],
-    tags: { location: "darkWood" },
+    tags: {},
     hierarchy: ["darkWood", "tower"],
   },
   raiseElevatorWithMillstone: {
     dependsOn: ["putMillstoneOnElevator"],
-    tags: { location: "darkWood" },
+    tags: {},
     hierarchy: ["darkWood", "tower"],
   },
   bindRopeToMillstone: {
     dependsOn: ["raiseElevatorWithMillstone", "getRope"],
-    tags: { location: "darkWood" },
+    tags: {},
     hierarchy: ["darkWood", "tower"],
   },
   bindRopeToTeeth: {
     dependsOn: ["getRope", "talkToDaughter"],
-    tags: { location: "darkWood" },
+    tags: {},
     hierarchy: ["darkWood", "tower"],
   },
   cutRopeOfElevator: {
     dependsOn: ["bindRopeToMillstone", "bindRopeToTeeth"],
-    tags: { location: "darkWood" },
+    tags: {},
     hierarchy: ["darkWood", "tower"],
   },
   pullToothOfDragon: {
     dependsOn: ["cutRopeOfElevator"],
-    tags: { location: "darkWood" },
+    tags: {},
     hierarchy: ["darkWood", "tower"],
   },
   getDragonTooth: {
     dependsOn: ["getIngredientList", "pullToothOfDragon"],
-    tags: { location: "darkWood" },
+    tags: {},
     hierarchy: ["darkWood", "tower"],
   },
   bringDaughterToBaker: {
     dependsOn: ["pullToothOfDragon"],
-    tags: { location: "darkWood" },
+    tags: {},
     hierarchy: ["darkWood", "tower"],
   },
   getApplePieFromBaker: {
     dependsOn: ["bringDaughterToBaker"],
-    tags: { location: "village" },
+    tags: {},
     hierarchy: ["village", "bakery"],
   },
   giveApplePieToDragon: {
     dependsOn: ["getApplePieFromBaker"],
-    tags: { location: "darkWood" },
+    tags: {},
     hierarchy: ["darkWood", "tower"],
   },
   getMoss: {
     dependsOn: ["getAccessToCabin"],
-    tags: { location: "forest" },
+    tags: {},
     hierarchy: ["forest"],
   },
   createPotionToTalkToDragon: {
     dependsOn: ["getRuneStone", "getMoss", "getAccessToCabin"],
-    tags: { location: "swamp" },
+    tags: {},
     hierarchy: ["swamp", "cabin"],
   },
   talkToDragon: {
     dependsOn: ["createPotionToTalkToDragon"],
-    tags: { location: "darkWood" },
+    tags: {},
     hierarchy: ["darkWood", "tower"],
   },
   getPaintingFromBasement: {
     dependsOn: ["talkToDragon"],
-    tags: { location: "darkWood" },
+    tags: {},
     hierarchy: ["darkWood", "tower"],
   },
   findSaddleInBasement: {
     dependsOn: ["getAccessToBasement"],
-    tags: { location: "darkWood" },
+    tags: {},
     hierarchy: ["darkWood", "tower"],
   },
   putSaddleOnDragon: {
@@ -409,12 +400,12 @@ g.definePuzzleDependencies<MetaData>({
       "findSaddleInBasement",
       "giveApplePieToDragon",
     ],
-    tags: { location: "darkWood" },
+    tags: {},
     hierarchy: ["darkWood", "tower"],
   },
   getGoldInForest: {
     dependsOn: ["followMoonStoneTrail"],
-    tags: { location: "forest", state: "text", languages: ["en", "nl"] },
+    tags: { state: "text", languages: ["en", "nl"] },
     hierarchy: ["forest"],
   },
   getMedicineToKing: {
