83ddc59d869d85ae39b079a328d3d3e7a0731070
Matthijs Groen <matthijs.groen@gmail.com>
2024-04-15 22:32:59 +0200

    Return to bakery

--- src/locations/bakery.ts
+++ src/locations/bakery.ts
@@ -2,31 +2,45 @@ import g from "../game";
 
 g.defineLocation("bakery", ({ describe, onEnter, onLeave, interaction }) => {
   describe(() => {
-    g.text(
-      "You are in the bakery. It is surprisingly empty.",
-      "No cakes, pies or bread."
-    );
-    g.text("The only thing for sale seem to be {b}cookies{/b}.");
+    g.onState(g.character("daughter").hasState("bakery"), () => {
+      g.text(
+        "You are in the bakery. {b}[characters.baker.name]{/b}, the baker, looks incredibly happy.",
+        "A smell of freshly baked bread and pies hangs in the air.",
+        "The daughter of the baker, {b}[characters.daughter.name]{/b}, is also here."
+      );
 
-    g.onState(
-      g.and(
-        g.character("dragon").hasFlag("toothPulled"),
-        g.not(g.character("baker").hasFlag("toldDaughter"))
-      ),
-      () => {
-        g.openOverlay("bakerConversation");
-      }
-    )
-      .else(g.location("bakery").hasFlag("visited"), () => {
-        g.text("The baker looks really sad.");
-      })
-      .else(() => {
-        g.text(
-          "You somehow expected to be greeted by the baker, but no luck.",
-          "The baker looks really sad."
-        );
-        g.location("bakery").setFlag("visited");
-      });
+      g.character("baker").say(
+        "Thank you so much for killing the dragon and saving my daughter!",
+        "I thought I would never see her again!"
+      );
+      g.text("{b}[characters.daughter.name]{/b} gives you a wink.");
+    }).else(() => {
+      g.text(
+        "You are in the bakery. It is surprisingly empty.",
+        "No cakes, pies or bread."
+      );
+      g.text("The only thing for sale seem to be {b}cookies{/b}.");
+
+      g.onState(
+        g.and(
+          g.character("dragon").hasFlag("toothPulled"),
+          g.not(g.character("baker").hasFlag("toldDaughter"))
+        ),
+        () => {
+          g.openOverlay("bakerConversation");
+        }
+      )
+        .else(g.location("bakery").hasFlag("visited"), () => {
+          g.text("The baker looks really sad.");
+        })
+        .else(() => {
+          g.text(
+            "You somehow expected to be greeted by the baker, but no luck.",
+            "The baker looks really sad."
+          );
+          g.location("bakery").setFlag("visited");
+        });
+    });
   });
 
   onEnter("village", () => {
